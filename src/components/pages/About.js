1a7048c249d0f1faea16807cee48b429fed26f4e
jakub stryja <jstryja@gmail.com>
2019-12-09 20:43:29 +0100

    route, About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is TodoList app based on React Crash Course
+        https://www.youtube.com/watch?v=sBws8MSXN7A&t=3872s
+      </p>
+    </React.Fragment>
+  );
+}
